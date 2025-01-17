aa7faf7525c0b0f29b5a689b73a27c6366c80d5d
Aldar Sugarov <aldarsugarof@gmail.com>
2025-01-18 00:34:46 +0900

    last17

--- Count Odd Numbers below n/CountOddNumbers.js
+++ Count Odd Numbers below n/CountOddNumbers.js
@@ -8,10 +8,10 @@
 
 function oddCount(n){
     let cnt = 0;
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i <= n; i++) {
         if (n % 2 === 1) {
             cnt++
             return cnt;
         }
     }
-}
+//
